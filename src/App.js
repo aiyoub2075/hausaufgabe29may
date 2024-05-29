dcc082f425fdf207f30a252a35228175acc179be
aiyoub2075 <aiyoub.reichert@edu.techstarter.de>
2024-05-29 15:48:01 +0200

    ff

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
+import { useState } from "react"
 import './App.css';
 
 function App() {
+
+  const[buttonColor, setButtonColor] = useState("rot")
+  const[buttonDesable, setButtonDesabled] = useState(false)
+  const nextColor = buttonColor === "rot" ? "blau" : "rot"
+  const nextDisabled = buttonDesable === false ? true : false
+  function farbeWechseln() {
+    setButtonColor(nextColor)
+  }
+  function chechBoxFunktion(){
+    setButtonColor("grau")
+    setButtonDesabled(nextDisabled)
+
+  }
+
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button
+        className = {buttonColor}
+        onClick={farbeWechseln}
+        disabled={buttonDesable}
+        >Aiyoub hat die farbe zu {nextColor} gewechselt
+      </button>
+      <br></br>
+      <input id = "checkbox" onChange={chechBoxFunktion}  type="checkbox"></input>
+      <label htmlFor="checkbox">Disable Button</label>
     </div>
   );
 }
